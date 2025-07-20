9d9791431180ec62521e7e0ee76184c2d63056b3
MichelGP <michel@greypixel.nl>
2025-07-20 10:53:42 +0200

    hoi

--- alloyExternal/check.cmd
+++ alloyExternal/check.cmd
@@ -5,9 +5,9 @@ kubectl get deployment alloy-deployment --namespace alloy-external
 ECHO ---------------------------
 kubectl get service alloy-service --namespace alloy-external
 ECHO ---------------------------
-kubectl get gateway otlp-gateway --namespace alloy-external
+kubectl get gateway alloy-gateway --namespace alloy-external
 ECHO ---------------------------
-kubectl get GRPCRoute otlp-grpc-route --namespace alloy-external
+kubectl get HTTPRoute alloy-http-route --namespace alloy-external
 ECHO ---------------------------
 kubectl describe gateway --namespace alloy-external
 ECHO ---------------------------
